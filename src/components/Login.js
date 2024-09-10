037b1d25639d85eba921669c29e2720fc4e741ea
Vishal Kumar <vkumar971660@gmail.com>
2024-09-10 20:10:10 +0530

    Validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { isValid } from "../utils/Validate";
 
 const Login = () => {
   const [togglForm, settoggleForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleClick = () => {
+    const message = isValid(email.current.value, password.current.value);
+    //console.log(email.current.value);
+    //console.log(password.current.value);
+    //console.log(message);
+    setErrorMessage(message);
+  };
 
   const handleToggleForm = () => {
     settoggleForm(!togglForm);
@@ -16,7 +29,10 @@ const Login = () => {
         src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg"
         alt="Bglogo"
       />
-      <form className=" absolute bg-black w-3/12 p-12 my-36 mx-auto right-0 left-0 bg-opacity-80">
+      <form
+        className=" absolute bg-black w-3/12 p-12 my-36 mx-auto right-0 left-0 bg-opacity-80"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="font-bold m-2 p-2 text-2xl text-white">
           {togglForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -37,19 +53,22 @@ const Login = () => {
 
         {togglForm && (
           <input
+            ref={email}
             className="w-full p-2 m-3 bg-slate-700 text-white"
             type="text"
             placeholder="User Name"
           ></input>
         )}
         <input
+          ref={password}
           className="w-full p-2 m-3 bg-gray-700 text-white"
           type="password"
           placeholder="Password"
         ></input>
-        <button className="bg-red-600 p-2 m-3 w-full">
+        <button className="bg-red-600 p-2 m-3 w-full" onClick={handleClick}>
           {togglForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="m-2 p-2 font-bold text-red-800">{errorMessage}</p>
         <p
           className="font-bold text-white hover: cursor-pointer"
           onClick={handleToggleForm}
---
+++ src/utils/Validate.js
@@ -0,0 +1,15 @@
+export const isValid = (email, password) => {
+  const isEmailValid =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  //console.log(email);
+  //console.log(password);
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(
+      password
+    );
+  if (!isEmailValid) return "Email is Not Valid";
+  if (!isPasswordValid) return "Password Is not valid";
+  return null;
+};
